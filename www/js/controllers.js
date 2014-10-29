4a269d7c9a096d4b7cea40b0659c62d53f29dc56
Tag0Mag0 <jem.hilton@gmail.com>
2014-10-29 17:15:37 -0500

    first implementation of ionic survey player
    forked from livewell_now code
    response capture not yet implemented

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -39,7 +39,7 @@ angular.module('starter.controllers', [])
   $scope.userRole = Session.userRole;
 })
 
-.controller('NewPatientsCtrl', function($scope, GuidMaker, $state) {
+.controller('NewPatientsCtrl', function($scope, GuidMaker, $state, $stateParams) {
 
   $scope.createParticipant = function(participant) {
     if(p.find("participants", {patient_identifier: participant.patient_identifier}).length == 0) {
@@ -59,21 +59,138 @@ angular.module('starter.controllers', [])
 
   $scope.reset();
 
-  $scope.formTemplate = {
-        "first": {
-            "type": "text",
-            "label": ""
+  $scope.pageTitle = 'Demographics';
+
+    //allows you to pass a question index url param into the question group directive
+    $scope.questionIndex = parseInt($stateParams.questionIndex)-1 || 0;
+
+    //overrides questiongroup default submit action to send data to PR
+    // $scope.submit = function(){
+
+        // var _SAVE_LOCATION = 'surveys';
+
+        // var responses = $('form').serializeArray();
+
+        // _.each(responses, function(el){
+
+
+        //   var payload = {
+        //     userId: UserDetails.find,
+        //     survey: $scope.pageTitle,
+        //     questionDataLabel: el.name,
+        //     questionValue: el.value,
+        //     sessionGUID: Guid.create(),
+        //     savedAt: new Date()
+        //   };
+
+        //   // (new PurpleRobot()).emitReading(_SAVE_LOCATION,payload).execute();
+        //   console.log(payload);
+
+        // });
+
+  $scope.questionGroups = [
+    {
+      content: "Include demographic information",
+      id: "d485d4c8d859f8cc",
+      order: 0,
+      questionDataLabel: "",
+      questionGroup: "demographics",
+      required: "",
+      responseGroupId: "",
+      responses: [],
+      type: "html"
+    },
+    {
+      content: "Over the past week, how often have you been bothered by LITTLE INTEREST OR PLEASURE IN DOING THINGS?",
+       id: "f99a845eea530b2c",
+       order: 1,
+       questionDataLabel: "demographics1",
+       questionGroup: "demographics",
+       required: "required",
+       responseGroupId: "demographics",
+       type: "radio",
+       responses: [
+        {
+          Objectid: "6f04febc1dcd99011",
+          label: "Not at all",
+          order: "1",
+          value: "0",
+          responseGroupId: "demographics",
+          responseGroupLabel: "demographics"
         },
-        "last": {
-            "type": "text",
-            "label": ""
+        {
+        Objectid: "6f04febc1dcd99012",
+          label: "Several days",
+          order: "2",
+          value: "2",
+          responseGroupId: "demographics",
+          responseGroupLabel: "demographics"
         },
-        "submit": {
-            "type": "submit",
-            "label": "submit",
-            "attributes": {"class": "button"}
+        {
+        Objectid: "6f04febc1dcd99013",
+          label: "More than half the days",
+          order: "3",
+          value: "3",
+          responseGroupId: "demographics",
+          responseGroupLabel: "demographics"
         },
-    };
+        {
+        Objectid: "6f04febc1dcd99014",
+          label: "Nearly every day",
+          order: "4",
+          value: "4",
+          responseGroupId: "demographics",
+          responseGroupLabel: "demographics"
+        }
+      ]
+
+    },
+    {
+      content: "Over the past week, how often have you been bothered by LITTLE INTEREST OR PLEASURE IN DOING THINGS?",
+       id: "f99a845eea530b2c",
+       order: 2,
+       questionDataLabel: "demographics1",
+       questionGroup: "demographics",
+       required: "required",
+       responseGroupId: "demographics",
+       type: "checkbox",
+       responses: [
+        {
+          Objectid: "6f04febc1dcd99011",
+          label: "Not at all",
+          order: "1",
+          value: "0",
+          responseGroupId: "demographics",
+          responseGroupLabel: "demographics"
+        },
+        {
+        Objectid: "6f04febc1dcd99012",
+          label: "Several days",
+          order: "2",
+          value: "2",
+          responseGroupId: "demographics",
+          responseGroupLabel: "demographics"
+        },
+        {
+        Objectid: "6f04febc1dcd99013",
+          label: "More than half the days",
+          order: "3",
+          value: "3",
+          responseGroupId: "demographics",
+          responseGroupLabel: "demographics"
+        },
+        {
+        Objectid: "6f04febc1dcd99014",
+          label: "Nearly every day",
+          order: "4",
+          value: "4",
+          responseGroupId: "demographics",
+          responseGroupLabel: "demographics"
+        }
+      ]
+
+    }
+  ];
 })
 
 .controller('LoginCtrl', function($scope, $rootScope, $state, DataService, AUTH_EVENTS, AuthService) {
---
+++ www/js/directives.js
@@ -0,0 +1,96 @@
+angular.module('starter.directives', [])
+
+.directive('survey', function ($location) {
+    return {
+      templateUrl: 'surveys/question_group.html',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+
+        scope.questionGroups = _.flatten(scope.$parent.questionGroups);
+
+        // -->  eliminate
+        scope._LABELS = scope.labels || [
+            {name:'back',label:'Back'},
+            {name:'next',label:'Next'},
+            {name:'submit', label:'Save'}
+        ];
+
+            scope._SURVEY_FAILURE_LABEL = scope.surveyFailureLabel || '<b>Unfortunately, this survey failed to load:</b>';
+
+
+
+            scope.surveyFailure = function(){
+
+                  var error = {};
+                  //there are no questions
+                  if (scope.questionGroups.length == 0 && _.isArray(scope.questionGroups)) {
+                        error = { error:true, message:"There are no questions available." }
+                  }
+                  //questions are not in an array
+                  else if (_.isArray(scope.questionGroups) == false){
+                        error = { error:true, message:"Questions are not properly formatted." }
+                  }
+                  else {
+                        error = { error:false }
+                  }
+
+                  if (error.error == true){
+                        console.error(error);
+                  }
+
+                  return error
+
+            }
+
+        scope.label = function(labelName){
+            return _.where(scope._LABELS, {name:labelName})[0].label
+        }
+
+        scope.numberOfQuestions = scope.questionGroups.length;
+
+
+        scope.currentIndex = scope.questionIndex || 0;
+
+        scope.showQuestion = function(questionIndex){
+            return questionIndex == scope.currentIndex;
+        }
+
+        scope.next = function(){
+            scope.currentIndex++;
+        };
+
+        scope.back = function(){
+            scope.currentIndex--;
+        };
+
+
+        //is overridden by scope.complete function if different action is desired at the end of survey
+        scope.submit = scope.submit || function(){
+            console.log('OVERRIDE THIS IN YOUR CONTROLLER SCOPE: ',$('form').serializeArray());
+            $location.path('#/');
+        }
+
+        scope.questionViewType = function(questionType){
+
+            switch (questionType){
+
+                case "radio":
+                return "radio"
+                break;
+                case "checkbox":
+                return "checkbox"
+                break;
+                case "text" || "phone" || "email" || "textarea":
+                return "single"
+                break;
+                default:
+                return "html"
+                break;
+
+            }
+
+        }
+
+      }
+    };
+  });
