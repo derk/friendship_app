f302358cc0d2856ac56aa3fee8196bfdd0891abf
Tag0Mag0 <jem.hilton@gmail.com>
2014-11-21 10:13:48 -0600

    changed ng-show to ng-if in survey directive templates for performance boost

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -19,6 +19,7 @@ angular.module('friendshipBench.controllers', [])
 
   $scope.lastSync = lastSync.get();
   $scope.$on('synced!', function(){
+    debugger;
     $scope.lastSync = lastSync.get();
   });
 })
